0ad83e4aace06364a640d958599cce320fd6746e
Bruno Gomes <bgomes@youongroup.com>
2020-03-09 18:09:47 +0000

    Updated

--- skoiy.py
+++ skoiy.py
@@ -2,9 +2,16 @@ import requests
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
+def merge(list1, list2): 
+      
+    merged_list = [(p1, p2) for idx1, p1 in enumerate(list1)  
+    for idx2, p2 in enumerate(list2) if idx1 == idx2] 
+    return merged_list 
+
 s = requests.Session() 
 
 loginUrl = 'https://accounts.youongroup.com/auth/login'
+chooseAccountUrl = 'https://accounts.youongroup.com/choose-account';
 
 webpage = urlopen(loginUrl).read()
 soup = BeautifulSoup(webpage, 'lxml')
@@ -14,22 +21,45 @@ results = soup.find('meta', attrs={'name':'csrf-token'})
 ## results = soup.find_all('meta', name='csrf-token')
 meta = soup.find_all("meta")
 csrfToken = results.attrs['content'];
-print(csrfToken)
+## print(csrfToken)
 
 
-r1 = requests.get(url='https://accounts.youongroup.com/auth/login')
+r1 = requests.get(url=loginUrl)
 print(r1)
 
 ## Parsing the response
 soup = BeautifulSoup(r1.text, 'lxml')
 results = soup.find('meta', attrs={'name':'csrf-token'})
 csrfToken = results.attrs['content'];
+## print(csrfToken)
+
+## Try to loggin
+payload = {'email': 'bgomes@youongroup.com', 'password': '1234567', '_token': csrfToken}
+r2 = requests.post(url=loginUrl, data=payload, cookies=r1.cookies)
+
+
+r4 = requests.get(url=chooseAccountUrl)
+soup = BeautifulSoup(r4.text, 'lxml')
+meta = soup.find_all("meta")
+csrfToken = results.attrs['content'];
 print(csrfToken)
 
-payload = {'email': 'bgomes@youongroup.com', 'password': '123456', '_token': csrfToken}
-r2 = requests.post(url="https://accounts.youongroup.com/auth/login", data=payload, cookies=r1.cookies)
-print(r2)
+## TODO List all the accounts and choose one
+soup = BeautifulSoup(r2.text, 'lxml')
+account_names = soup.findAll('input', attrs={'name':'account_name'})
+account_tokens = soup.findAll('input', attrs={'name':'account_token'})
+names = [ tag.attrs['value'] for tag in account_names]
+tokens = [ tag.attrs['value'] for tag in account_tokens]
+merged_list = merge(names, tokens)
 
+## print(merged_list)
+
+## TODO Choose the account and proceed
+## txt = input("Choose your account please: ")
+## print("Is this what you just said? ", txt)
+payload = { 'account_token' : '5nff7dqpce5u4moq', '_token': csrfToken}
+r3 = requests.post(url=chooseAccountUrl, data=payload, cookies=r4.cookies)
+print(r3)
 
 ## <meta name="csrf-token" content="PalnibjMJNASZ8eQC8KrNzXYAfMj7E6xvc4yp75i">
 ## <input type="hidden" name="_token" value="PalnibjMJNASZ8eQC8KrNzXYAfMj7E6xvc4yp75i">
