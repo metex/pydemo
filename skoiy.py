adc972886d53baa596172117f5011e1c7fa968fe
Bruno Gomes <bgomes@youongroup.com>
2020-03-10 13:08:10 +0000

    Updated

--- skoiy.py
+++ skoiy.py
@@ -1,22 +1,49 @@
+import pickle
+import hashlib
 from lib.skoiy import *
+from datetime import datetime
 
 def main():
-    
-    r1 = tryToLogin("bgomes@youongroup.com", "1234567")
-    print(r1.text)
+
+    email = input("Please enter your email: ")
+    password = input("Please enter your password: ")
+
+    email = "bgomes@youongroup.com"
+    password = "1234567"
+
+    # Generate the cookie hash
+    m = hashlib.md5()
+    m.update((email+password).encode('UTF-8'))
+    checksum = m.hexdigest()
+    print(checksum)
+
+    # Load session
+    with open('storage/cookies', 'rb') as f:
+        s.cookies.update(pickle.load(f))
+
+    expires = next(x for x in s.cookies if x.name == 'sts').expires
+    dt_object = datetime.fromtimestamp(expires)
+    print(dt_object)
+
+    # r1 = tryToLogin("bgomes@youongroup.com", "1234567")
+    # print(r1.text)
     # expires = next(x for x in r1.cookies if x.name == 'sts').expires
     # is_expired = next(x for x in r1.cookies if x.name == 'sts').is_expired()
     # print(is_expired)
 
-    ## accounts = listAccounts(tryToLogin("bgomes@youongroup.com", "1234567"))
-    ## print(accounts)
+    # accounts = listAccounts(tryToLogin(email, password))
+    # print(accounts)
 
     # token = "5nff7dqpce5u4moq"
     # r2 = chooseAccount(r1, token)
     # getVideos(r2)
 
+    # Save session    
+    # with open('storage/hash', 'wb') as f:
+    #    pickle.dump(s.cookies, f)
+
     # Start with an empty list. You can 'seed' the list with
-    #  some predefined values if you like.
+    # some predefined values if you like.
     names = []
 
     # Set new_name to something other than 'quit'.
